e33a4ee4b277b4bcd9f5437c3b39f931619a7c58
elenaBushueva <helenabushueva@gmail.com>
2021-09-21 22:03:14 -0700

    innitial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [totalAmount, setTotalAmount] = useState('');
+    const [downPayment, setDownPayment] = useState('');
+    const [years, setYears] = useState('');
+    const [report, setReport] = useState([]);
+
+
+    const onChangeTotalAmount = (e) => {
+        setTotalAmount(e.target.value);
+    };
+    const onChangeDownPayment = (e) => {
+        setDownPayment(e.target.value);
+    };
+    const onChangeYears = (e) => {
+        const updatedYear = e.target.value;
+        setYears(updatedYear);
+
+        let newReport = [];
+        const payment = (totalAmount - downPayment) / updatedYear;
+        let debt = totalAmount - downPayment;
+
+        for (let i = 1; i <= updatedYear; i++) {
+            debt -= payment;
+            newReport.push({year: i, payment, debt});
+        }
+        setReport(newReport);
+    }
+
+    return (
+        <div>
+            <label htmlFor="totalAmount">Total Amount :</label>
+            <input id="totalAmount" type="number" value={totalAmount} onChange={onChangeTotalAmount}/>
+            <br/><br/>
+
+            <label htmlFor="downPayment">Down payment :</label>
+            <input id="downPayment" type="number" value={downPayment} onChange={onChangeDownPayment}/>
+            <br/><br/>
+
+            <label htmlFor="downPayment">Years :</label>
+            <input id="yars" type="number" value={years} onChange={onChangeYears}/>
+            <br/><br/>
+
+            <hr/>
+
+            Total Amount : {totalAmount} <br/>
+            Down payment : {downPayment} <br/>
+            Credit : {totalAmount - downPayment} <br/>
+            Down payment, % : {(downPayment / totalAmount * 100).toFixed(2)}
+            <br/> <br/>
+
+            <table border={1}>
+                <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Payment</th>
+                    <th>Debt</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                {report.map(el =>
+                    <tr key={el.year}>
+                        <td>{el.year}</td>
+                        <td>{el.payment.toFixed()}</td>
+                        <td>{el.debt.toFixed()}</td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
+
+        </div>
+    );
+};
 
 export default App;
